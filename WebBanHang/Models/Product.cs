686b189f5ac784719e77f1d0508ccb7a9f455cc7
ADMIN <ADMIN@PC316>
2025-05-16 11:32:29 +0700

    01

--- WebBanHang/Models/Product.cs
+++ WebBanHang/Models/Product.cs
@@ -11,11 +11,11 @@ namespace WebBanHang.Models
         public int Id { get; set; }
         [Required,StringLength(200)]
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
-        public string? ImageUrl { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -52,7 +52,7 @@ namespace WebBanHang
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }
